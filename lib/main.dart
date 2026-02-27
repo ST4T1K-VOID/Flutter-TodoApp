9a4ee999233f3f061c167ca3f6574a5c5bbc758c
ST4T1K <mjobrien06@hotmail.com>
2026-02-27 09:27:17 +0800

    feat: display todos list

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo/models/todo.dart';
 
-void main() {}
+void main() {
+  runApp(TodoApp());
+}
 
 class TodoApp extends StatelessWidget {
   const TodoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: "Todo App",
+      home: TodoHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class TodoHomePage extends StatefulWidget {
+  const TodoHomePage({super.key});
+
+  @override
+  State<TodoHomePage> createState() => _TodoHomePageState();
+}
+
+class _TodoHomePageState extends State<TodoHomePage> {
+  final List<Todo> todos = <Todo>[
+    Todo(name: "Shopping", description: "Pick up groceries"),
+    Todo(name: "Paint", description: "Recreate the Mona Lisa"),
+    Todo(name: "Dance", description: "I wanna dance with somebody"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('TODO APPBAR')),
+      body: Center(
+        child: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (BuildContext context, int i) {
+            return Container(
+              padding: const EdgeInsets.all(5),
+              child: Text(todos[i].toString()),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
