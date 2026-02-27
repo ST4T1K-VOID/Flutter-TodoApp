17ff499530567e3b1af691b1cdd34019db895bb0
ST4T1K <mjobrien06@hotmail.com>
2026-02-27 09:27:02 +0800

    feat: add todo class

---
+++ lib/models/todo.dart
@@ -0,0 +1,13 @@
+class Todo {
+  final String name;
+  final String description;
+  final bool complete;
+
+  Todo({required this.name, required this.description, this.complete = false});
+
+  @override
+  String toString() {
+    // cocncatenates `name` and `description` properties together
+    return "$name - ($description)";
+  }
+}
