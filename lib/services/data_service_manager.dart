5d1536669f25de4d708b7a4f1073038322c8202f
ST4T1K <mjobrien06@hotmail.com>
2026-04-07 14:57:49 +0800

    feat(wip): session 9

---
+++ lib/services/data_service_manager.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/foundation.dart';
+import 'dart:html' as html;
+
+import './hive_datasource.dart';
+import './sqlite_datasource.dart';
+import './remote_datasource.dart';
+import './datasource.dart';
+
+class DataServiceManager {
+  late final IDataSource _local; //set in constructor
+  final IDataSource _remote = RemoteDatasource();
+
+  DataServiceManager() {
+    if (kIsWeb) {
+      _local = HiveDatasource();
+    } else {
+      _local = SqliteDatasource();
+    }
+  }
+
+  Future<bool> _isOnline() async {
+    //
+  }
+}
